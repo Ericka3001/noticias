366f2ec9076b9102062c70550973022e26b2010f
Ericka <erickaerguetabasco@gmail.com>
2020-01-25 01:02:14 -0400

    Desplegar artículos

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,5 +1,6 @@
 import { Component,OnInit } from '@angular/core';
 import { NoticiasService } from 'src/app/services/noticias.service';
+import { Article } from 'src/app/interfaces/interfaces';
 
 @Component({
   selector: 'app-tab1',
@@ -8,15 +9,23 @@ import { NoticiasService } from 'src/app/services/noticias.service';
 })
 export class Tab1Page implements OnInit {
 
+  noticias: Article []=[];   // es el que se recorrerá en el lado del html
+
   constructor( private noticiasService:NoticiasService) {
 
   }
-
+  
   ngOnInit() {
     //llamamos el servicio
     this.noticiasService.getTopHeadLines().
       subscribe(resp =>{
         console.log ('noticias',resp) // visualizar los datos en la consola  
+       // this.noticias=resp.articles ;  
+       //eventualmente se trae mas información y no se desea sobreescribir los artículos
+
+        // los 3 puntos se usan para que se trabaje de forma independiente
+        // cada elemento del arreglo que será desplegado en el HTML 
+        this.noticias.push( ...resp.articles ); 
       });
 
   }
